9ed0e9f4415006c8b4983747fdd8eb3602cb50fc
Arth K. Gajjar <iarthstar@gmail.com>
2024-01-02 16:08:48 +0530

    updated : User Signup Webhook with Telegram Bot SendMessage

--- app/api/webhooks/users/route.ts
+++ app/api/webhooks/users/route.ts
@@ -1,66 +1,3 @@
-// import { Webhook } from 'svix'
-// import { headers } from 'next/headers'
-// import { WebhookEvent } from '@clerk/nextjs/server'
-
-// export async function GET(req: Request) {
-//   return new Response('', { status: 200 });
-// }
-
-// export async function POST(req: Request) {
-
-//   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-//   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
-
-//   if (!WEBHOOK_SECRET) {
-//     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
-//   }
-
-//   // Get the headers
-//   const headerPayload = headers();
-//   const svix_id = headerPayload.get("svix-id");
-//   const svix_timestamp = headerPayload.get("svix-timestamp");
-//   const svix_signature = headerPayload.get("svix-signature");
-
-//   // If there are no headers, error out
-//   if (!svix_id || !svix_timestamp || !svix_signature) {
-//     return new Response('Error occured -- no svix headers', {
-//       status: 400
-//     })
-//   }
-
-//   // Get the body
-//   const payload = await req.json()
-//   const body = JSON.stringify(payload);
-
-//   // Create a new Svix instance with your secret.
-//   const wh = new Webhook(WEBHOOK_SECRET);
-
-//   let evt: WebhookEvent
-
-//   // Verify the payload with the headers
-//   try {
-//     evt = wh.verify(body, {
-//       "svix-id": svix_id,
-//       "svix-timestamp": svix_timestamp,
-//       "svix-signature": svix_signature,
-//     }) as WebhookEvent
-//   } catch (err) {
-//     console.error('Error verifying webhook:', err);
-//     return new Response('Error occured', {
-//       status: 400
-//     })
-//   }
-
-//   // Get the ID and type
-//   const { id } = evt.data;
-//   const eventType = evt.type;
-
-//   console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-//   console.log('Webhook body:', body)
-
-//   return new Response('', { status: 200 })
-// }
-
 //ignore typescript
 // @ts-nocheck
 import type { User } from "@clerk/nextjs/api";
@@ -156,6 +93,24 @@ export async function POST(req) {
         })
         .select();
       console.log(data, error);
+
+      // Notify Mannat Signup using Telegram Bot
+      fetch(
+        "https://api.telegram.org/bot" +
+          process.env.TELEGRAM_BOT_TOKEN +
+          "/sendMessage",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            chat_id: process.env.TELEGRAM_CHAT_ID_MANNAT_SIGNUP,
+            text: `New user ${evt.data.first_name} ${evt.data.last_name} with Email: ${emailObject?.email_address} just signed up!`,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
       return new Response(JSON.stringify(data));
     } catch (error) {
       console.log(error);
